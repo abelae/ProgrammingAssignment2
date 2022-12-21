1894d2fbc05b5c02fdab3dc851ce1b4d123ae6b1
abelae <117683397+abelae@users.noreply.github.com>
2022-12-21 13:28:44 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function creates a chacheable function for inputing matrixes and their inverse
 
+makeCacheMatrix <- function(cachedMat = matrix()) {
+  cachedInv <- NULL ## inverse
+  setMat <- function(matVal = matrix()) {
+    cachedMat <<- matVal 
+    cachedInv <<- NULL
+  }
+  
+  getMat <- function() cachedMat
+  
+  ##setMat inverse variable in parent env to desired value and return the value as a convenience
+  setInv <- function(invVal) {
+    cachedInv <<- invVal 
+    return(cachedInv)
+  }
+  
+  getInv  <- function() cachedInv
+  list(setMat=setMat, getMat=getMat, setInv=setInv, getInv=getInv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachedMat=usrVal, ...) { ##special matrix provided or create a test 2x2 matrix
+  
+  calculatedInverse <- cachedMat$getInv() 
+  if(!is.null(calculatedInverse)){
+    if(is.matrix(calculatedInverse)){
+      print("Cached matrix already exists:")
+      calculatedInverse
+    }
+  }
+  else{
+    matrixToSolve <- cachedMat$getMat()
+    cachedMat$setMat
+    calculatedInverse <- solve(matrixToSolve)
+    print("Your inverse matrix is:")
+    cachedMat$setInv(calculatedInverse)
+  }
+
+
+ 
 }
+
+abelaux = makeCacheMatrix(matrix(data = c(4,3,2,5), nrow=2, ncol=2))
+cacheSolve(abelaux)
+## Type twice to run
+cacheSolve(abelaux)
+
